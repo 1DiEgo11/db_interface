4e492bca60904a423f70f39f4894e79728ae2e68
1DiEgo11 <sona.egor23@gmail.com>
2023-04-15 21:47:37 +1000

    Добавьте файлы проекта.

---
+++ data_base/Program.cs
@@ -0,0 +1,4 @@
+﻿using Interface;
+
+ConsoleRectangle.Draw(25, 5, 70, 20, ConsoleColor.White);
+
---
+++ interface/Class1.cs
@@ -0,0 +1,37 @@
+﻿using System.Drawing;
+
+namespace Interface
+{
+    public class ConsoleRectangle
+    {
+       
+        public static void Draw(int x, int y, int sizeX, int sizeY, ConsoleColor BorderColor)
+        {
+
+            
+            string s = "╔";
+            string space = "";
+            string temp = "";
+            for (int i = 0; i < sizeX; i++)
+            {
+                space += " ";
+                s += "═";
+            }
+            for (int j = 0; j < x; j++)
+                temp += " ";
+            s += "╗" + "\n";
+            for (int i = 0; i < sizeY; i++)
+                s += temp + "║" + space + "║" + "\n";
+            s += temp + "╚";
+            for (int i = 0; i < sizeX; i++)
+                s += "═";
+            s += "╝" + "\n";
+            Console.ForegroundColor = BorderColor;
+            Console.CursorTop = y;
+            Console.CursorLeft = x;
+            Console.Write(s);
+            Console.ResetColor();
+        }
+    }
+}
+
